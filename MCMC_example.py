0103e62bc9521f5e046801ad29ef49da8fa4c19b
Aiden Gundersen <aidengundersen36@gmail.com>
2024-03-15 01:07:43 -0500

    "added comments for example"

--- MCMC_example.py
+++ MCMC_example.py
@@ -4,15 +4,16 @@ from time import time
 
 
 ###################################################################
-################## CONSTRUCT MODEL  ###############################
+################## CONSTRUCT MODEL ################################
 ###################################################################
 
-
+# define number of parameters, minimum and maximum values, and labels
 num_params = 2
 param_mins = [-5., -5.]
 param_maxs = [5., 5.]
 param_labels = ['x', 'y']
 
+# returns true if parameters are in allowed domain, otherwise false
 def in_domain(params):
     in_dom = True
     for i in range(num_params):
@@ -21,10 +22,12 @@ def in_domain(params):
             continue
     return in_dom
 
+# ln(likelihood) function
 def lnlike_func(params):
     x, y = params
     return np.log(16/(3*np.pi)*(np.exp(-x**2-(9+4*x**2+8*y)**2)+1/2*np.exp(-8*x**2-8*(y-2)**2)))
 
+# ln(prior) function
 def lnprior_func(params):
     for k in range(len(params)):
         if params[k] < param_mins[k] or params[k] > param_maxs[k]:
@@ -32,7 +35,7 @@ def lnprior_func(params):
     else:
         return 0
 
-
+# construct model object
 model = Model(num_params, param_mins, param_maxs, param_labels, in_domain, lnlike_func, lnprior_func)
 
 
@@ -40,12 +43,13 @@ model = Model(num_params, param_mins, param_maxs, param_labels, in_domain, lnlik
 ######################## DO MCMC ##################################
 ###################################################################
 
-
+# define jump blend, number of chains, length of history, and number of samples to draw
 jump_blend = [0.5, 0.5]
 num_chains = 5
 len_history = 1_000
 num_samples = int(1e5)
 
+# construct MCMC object
 mcmc = MCMC(model, num_samples, jump_blend, num_chains, len_history)
 
 # do MCMC!
@@ -64,10 +68,16 @@ params_injs = [[0., -9./8.], [0., 2.]]
 burnin = int(num_samples / 10)
 pp = PostProcessing(model, chains, mcmc, params_injs)
 
+# print acceptance fractions
 print('acceptance fraction per chains:')
 print(pp.get_acc_frac())
+
+# print maximum a posteriori (MAP) parameters
 print('MAP parameters:')
 print(pp.get_MAP())
+
+# make trace plot, plot posterior values, and corner plot
 pp.plt_trace()
 pp.plt_lnlikes()
 pp.plt_corner(burnin)
+
